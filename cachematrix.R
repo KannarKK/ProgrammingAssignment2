20bc21bf9d0f4bf65180644f07e10f5c3144c154
Kiran Kannar <kirankannarkk.lris@gmail.com>
2015-07-25 19:37:52 +0530

    Kiran's next commit

--- cachematrix.R
+++ cachematrix.R
@@ -5,10 +5,13 @@
 ## to a class which defines member variables and functions.
 
 ## makeCacheMatrix provides the following four functions i.e. methods
-## set - set the value of the matrix
-## get - get the value of the matrix 
-## setinv - set the value of the inverse matrix with the given matrix
-## getinv - get the value of the inverse matrix 
+## set() - set the value of the matrix
+## get() - get the value of the matrix 
+## setinv() - set the value of the inverse matrix with the given matrix
+## getinv() - get the value of the inverse matrix 
+
+##Note 0: This method therefore acts as a caching mechanism for inverse matrices.
+##Note 1: This assumes that matrix is invertible i.e. matrix is square matrix (N*N)
 
 makeCacheMatrix <- function(x = matrix()) {
  inverse <- NULL
@@ -25,9 +28,10 @@ makeCacheMatrix <- function(x = matrix()) {
  list(set=set, get=get, getinverse=getinverse,setinverse=setinverse)
 }
 
-## cacheSolve is a function which calculate the inverse of a matrix created with
-## makeCacheMatrix and store it in the structure. If the matrix is not changed
-## every nex invoke of cacheSolve just reads the stored inverse matrix. 
+## cacheSolve is a function which receives an object returned by makeCacheMatrix().
+##It then goes on to verify if the inverse is already calculated or not.
+##If inverse is already calculated, it just retrieves the set inverse matrix.
+##Else, it calculates and returns the inverse matrix using solve() function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -37,7 +41,18 @@ cacheSolve <- function(x, ...) {
       return(m)
   }
   mat <- x$get()
-  inverse <- solve(mat)
+  inverse <- solve(mat)   ##solve() is used to calculate the inverse.
   x$setinverse(inverse)
   inverse
 }
+
+
+##To use the above functions, see the below example.
+## x<- matrix(c(2,4,1,3),2,2)
+##  2  1
+##  4  3
+## mycache <- makeCacheMatrix(x)
+## cacheSolve(mycache)
+## Output inverse matrix:
+##  1.5 -0.5
+## -2.0  1.0
